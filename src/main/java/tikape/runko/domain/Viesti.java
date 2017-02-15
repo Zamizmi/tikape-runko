c1a17d583c7471585c8a372b426d688103b56cb6
Zamizmi <sami.k.lindqvist@gmail.com>
2017-02-15 17:45:18 +0200

    viesti.java created

---
+++ src/main/java/tikape/runko/domain/Viesti.java
@@ -0,0 +1,71 @@
+package tikape.runko.domain;
+
+import java.sql.Timestamp;
+
+
+/**
+ *
+ * @author saklindq
+ */
+public class Viesti {
+    
+    private int id;
+    private int vastattu_viesti;
+    private int ketju;
+    private String nimimerkki;
+    private String sisalto;
+
+    public Viesti(int id, int vastattu_viesti, int ketju, String nimimerkki, String sisalto) {
+        this.id = id;
+        this. vastattu_viesti = vastattu_viesti;
+        this.ketju = ketju;
+        this.nimimerkki = nimimerkki;
+        this.sisalto = sisalto;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+    
+    public void setId(int avain) {
+        this.id = avain;
+    }
+
+    public int getKetju() {
+        return ketju;
+    }
+
+    public int getVastattu_viesti() {
+        return vastattu_viesti;
+    }
+    
+    public String getNimimerkki() {
+        return nimimerkki;
+    }
+
+    public String getSisalto() {
+        return sisalto;
+    }
+
+    public void setKetju(int ketju) {
+        this.ketju = ketju;
+    }
+
+    public void setNimimerkki(String nimimerkki) {
+        this.nimimerkki = nimimerkki;
+    }
+
+    public void setSisalto(String sisalto) {
+        this.sisalto = sisalto;
+    }
+
+    public void setVastattu_viesti(int vastattu_viesti) {
+        this.vastattu_viesti = vastattu_viesti;
+    }
+
+    @Override
+    public String toString() {
+        return this.sisalto;
+    }
+        
+}
